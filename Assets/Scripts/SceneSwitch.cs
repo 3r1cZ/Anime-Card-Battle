c8342a03ba72786eb74e1b447be3fa3dc9e71ed6
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-06-10 09:46:17 -0400

    Add events for remaining buttons and create settings and lobby scenes

--- Assets/Scripts/SceneSwitch.cs
+++ Assets/Scripts/SceneSwitch.cs
@@ -5,9 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
 {
+    // switches scene from MainMenu to Rules
     public void switchToRules()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    // switches scene from Rules to MainMenu
+    public void rulesToMainMenu() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    
+    // switches scene from MainMenu to Settings
+    public void switchToSettings() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    // switches scene from Settings to MainMenu
+    public void settingsToMainMenu() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+    // switches scene from MainMenu to Lobby
+    public void switchToLobby() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    // switches scene from Rules to Lobby
+    public void rulesToLobby() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
 }
