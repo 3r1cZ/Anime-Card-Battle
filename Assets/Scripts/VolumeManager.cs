873bec20ae869d13a44eef4a09902b12281dceac
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-06-11 13:46:33 -0400

    Fix volume adjuster and slider UI

--- Assets/Scripts/VolumeManager.cs
+++ Assets/Scripts/VolumeManager.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     public AudioMixer _audio;
+    public Slider musicSlider;
 
+    void Start()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("vol", 0.75f);
+    }
     public void setVolume(float vol) {
-        _audio.SetFloat("vol", vol);
+        _audio.SetFloat("vol", Mathf.Log10(vol) * 20);
+        PlayerPrefs.SetFloat("vol", vol);
     }
 }
