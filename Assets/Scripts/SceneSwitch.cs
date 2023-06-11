6f3440561a0b0f013508055b1b42e6bda7e6d2d1
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-06-11 16:40:27 -0400

    Reorganize code to fit one scene
    + add SFX volume adjuster

--- Assets/Scripts/SceneSwitch.cs
+++ Assets/Scripts/SceneSwitch.cs
@@ -6,38 +6,9 @@ using UnityEngine.SceneManagement;
 public class SceneSwitch : MonoBehaviour
 {
     // switches scene from MainMenu to Rules
-    public void switchToRules()
+    public void SwitchToGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // switches scene from Rules to MainMenu
-    public void rulesToMainMenu() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-    }
-    
-    // switches scene from MainMenu to Settings
-    public void switchToSettings() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
-    }
-
-    // switches scene from Settings to MainMenu
-    public void settingsToMainMenu() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
-    }
-
-    // switches scene from MainMenu to Lobby
-    public void switchToLobby() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
-    }
-
-    // switches scene from Rules to Lobby
-    public void rulesToLobby() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
-    }
-
-    // switches scene from Lobby to MainMenu
-    public void lobbyToMainMenu() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
-    }
 }
